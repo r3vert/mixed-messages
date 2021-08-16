341e03f654c5a03b67d2b2152d5ee014bc19ac48
Keenan Homsi <keenanhomsi@gmail.com>
2021-08-16 18:05:50 -0400

    reformated code

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ const _ = require('lodash')
 const beginningArray = ["You should", "You shouldn't", "You could", "You can", "You cannot"]
 const thingsArray = ["code", "bike", "swim", "walk", "run", "play", "work"]
 const whenArray = ["now", 'later', 'soon', 'everyday', 'for a day', 'for a month', 'for a year']
-const finnishedString = `${_.sample(beginningArray)} ${_.sample(thingsArray)} ${_.sample(whenArray)}`
-console.log(finnishedString)
+const randomArrayString = (array1, array2, array3) => {
+    return `${_.sample(array1)} ${_.sample(array2)} ${_.sample(array3)}`
+}
+console.log(randomArrayString(beginningArray,thingsArray,whenArray))
