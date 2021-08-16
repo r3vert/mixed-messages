e7a0486eaca8e15b2eec573d3aff538a802ad989
Keenan Homsi <keenanhomsi@gmail.com>
2021-08-16 18:03:11 -0400

    added finnished string and logged it

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ const _ = require('lodash')
 const beginningArray = ["You should", "You shouldn't", "You could", "You can", "You cannot"]
 const thingsArray = ["code", "bike", "swim", "walk", "run", "play", "work"]
 const whenArray = ["now", 'later', 'soon', 'everyday', 'for a day', 'for a month', 'for a year']
+const finnishedString = `${_.sample(beginningArray)} ${_.sample(thingsArray)} ${_.sample(whenArray)}`
+console.log(finnishedString)
